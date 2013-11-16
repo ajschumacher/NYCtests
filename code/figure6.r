6c65e65f3290d193ecb732a71bad1847612dcdd4
Aaron Schumacher <ajschumacher@gmail.com>
2013-11-16 15:49:06 -0500

    figure 6a and 6b (showing D75 results)

--- code/figure6.r
+++ code/figure6.r
@@ -19,12 +19,25 @@ data$d75 <- substr(data$dbn, 1, 2) == "75"
 
 library(ggplot2)
 
-#png(width=1280, height=720, filename="../figure/2.png")
+png(width=1280, height=720, filename="../figure/6a.png")
 ggplot(subset(data, subject=="Math")) + aes(x=score, fill=d75) +
   guides(fill=FALSE) + scale_fill_manual(values=c("steelblue", "red")) +
   geom_histogram(binwidth=1) +
   facet_grid(year ~ grade) +
   xlab("average score as reported by NYCDOE") +
   ylab("number of schools reporting such an average for the grade") +
-  ggtitle("Figure 2. All reported average math scores for NYC public schools (charter and non-charter) grades 3-8, 2006-2013")
-#dev.off()
+  ggtitle("Figure 6a. All reported average Math scores for NYC public schools (charter and non-charter) grades 3-8, 2006-2013\n(District 75 school results are in red; all other schools' results are in blue.)\n")
+dev.off()
+
+png(width=1280, height=720, filename="../figure/6b.png")
+ggplot(subset(data, subject=="ELA")) + aes(x=score, fill=d75) +
+  guides(fill=FALSE) + scale_fill_manual(values=c("steelblue", "red")) +
+  geom_histogram(binwidth=1) +
+  facet_grid(year ~ grade) +
+  xlab("average score as reported by NYCDOE") +
+  ylab("number of schools reporting such an average for the grade") +
+  ggtitle("Figure 6b. All reported average ELA scores for NYC public schools (charter and non-charter) grades 3-8, 2006-2013\n(District 75 school results are in red; all other schools' results are in blue.)\n")
+dev.off()
+
+# check that there aren't any 2013 D75 results at all
+stopifnot(sum(data$year==2013 & data$d75==TRUE) == 0)
